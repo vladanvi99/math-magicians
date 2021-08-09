1176ef5c1ee2275c7b7b26a0dce38dc6053dc3c8
unknown <videnovic99@hotmail.com>
2021-08-09 11:08:36 +0200

    fix-linters

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,7 +1,7 @@
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ 
-      getCLS, getFID, getFCP, getLCP, getTTFB 
+    import('web-vitals').then(({
+      getCLS, getFID, getFCP, getLCP, getTTF,
     }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
